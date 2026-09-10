d79a042b4dc4b2ee4409fbded1a5d7fb482dba01
crealu <cameron.miller294@gmail.com>
2026-09-09 17:37:21 -0700

    Create sayo

---
+++ sayo.js
@@ -0,0 +1,130 @@
+let songs = [
+	{
+		name: 'Joanna',
+		key: 'A',
+		parts: [
+			[
+				'B', 'A', 'G♯', 'F♯', 'E', 'D♯', 'B', '|'
+			],
+			[
+				'solo', '|'
+			]
+		]
+	},
+	{
+		name: 'Campus Girl',
+		key: 'F',
+		parts: [
+			[
+				'E', 'F♯', 'G', 'B', 'A', '|'
+			]
+		]
+	},
+	{
+		name: 'Mayonaka',
+		key: 'F',
+		parts: [
+			[
+
+			]
+		]
+	},
+	{
+		name: 'Last Summer Whisper',
+		key: 'A',
+		parts: [
+			[
+				'A♯', 'G♯', 'G♯', '|',
+				'G♯', 'F♯', 'F', 'F♯', 'G♯', 'F♯', 'F', '|',
+				'C♯', 'A♯', '|',
+				'A♯', 'G♯', 'G♯', '|',
+				'G♯', 'F♯', 'F', 'F♯', 'G♯', 'F♯', 'F', '|',
+				'D♯', 'F', 'D', 'C♯', '|'
+			],
+			[
+				'C♯', '(5x)', 'B', 'F♯', '|',
+				'F♯', 'G♯', 'A♯', 'A♯', 'A♯', 'B', 'A♯', 'B', 'A♯', '|',
+				'A♯', 'A♯', 'F♯', 'A♯', 'F♯', 'G♯', '(A)', 'A♯', '|',
+				'C', 'C♯', '(4x)', 'B', 'F♯', '|',
+				'F♯', 'G♯', 'A♯', 'A♯', 'A♯', 'B', 'A♯', 'B', 'A♯', '|',
+				'A♯', 'A♯', 'F♯', 'A♯', 'F♯', 'G♯', '(A)', 'A♯', '|'
+			],
+			[
+				'(omo sugita)', '|',
+				'G♯', 'A♯', 'B', 'F♯', 'F', 'F♯', 'F', '', 'D♯', '|',
+				'B', 'G♯', 'F', 'F♯', 'F', '|',
+				'B', 'B', 'A♯', 'F♯', 'D♯', 'F♯', 'G♯', '(F♯)', 'F', '|'
+			]
+		]
+	},
+	{
+		name: 'Love Was Really Gone',
+		key: '',
+		parts: [
+			[
+				'A', '|',
+				'strings part at end with changed key?', '|'
+			]
+		]
+	},
+	{
+		name: 'Midnight Pretenders',
+		key: '',
+		parts: [
+			[
+				'A', '|',
+				'key change around 5:00 to end song', '|'
+			]
+		]
+	},
+	{
+		name: 'Kanashimi Ga Tomaranai',
+		key: 'A',
+		parts: [
+			[
+				'F♯', 'G♯', 'A', 'B', '|',
+				'C♯', 'B', 'A', 'G♯', 'F♯', '|'
+			]
+		]
+	},
+	{
+		name: 'Matsuri',
+		key: 'E',
+		parts: [
+			[
+				'D♯', '|',
+				'D♯', 'C♯', 'B', 'A♯', 'G♯', 'C♯', 'A♯' 'G♯', '|'
+			],
+			[
+				'solo flute part', '|'
+			]
+		]
+	},
+	{
+		name: 'Plastic Love',
+		key: 'F',
+		parts: [
+			[
+				'E', '|',
+				'B', 'D', 'E', '(3x)', '|',
+				'G', 'B', 'F♯', 'A', 'E', 'G', 'D', 'F♯', '|'
+			],
+			[
+				'E', 'E', 'D', 'E', '|',
+				'F♯', 'F♯', 'E', 'F♯', '|',
+				'G', '|'
+			],
+			[
+				'G', 'G', 'F♯', 'G', '|', 
+				'F♯', 'G', 'B', 'F♯', 'G', 'B', '|'
+			],
+			[
+				'B', 'F♯', 'G', 'A', 'B', 'F♯', '|'
+			],
+			[
+				'B', 'B', 'A', 'B', '', 'G', 'A', 'B', '|',
+				'E', '|'
+			]
+		]
+	}
+]
